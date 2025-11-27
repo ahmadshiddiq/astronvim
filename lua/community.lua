8af07a7594ef4d813f204334b2fcff9fbc2d1c91
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2025-11-27 13:05:48 +0700

    feat: add new plugins to communitty.lua

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,23 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- PACK
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.java" },
+  -- LSP
+  { import = "astrocommunity.lsp.nvim-java" },
+  -- COMPLETION
+  { import = "astrocommunity.completion.supermaven-nvim" },
+  -- EDITING SUPPORT
+  { import = "astrocommunity.editing-support.vim-visual-multi" },
+  -- MOTION
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-jump" },
+  -- MISC
+  { import = "astrocommunity.motion.nvim-surround" },
 }
